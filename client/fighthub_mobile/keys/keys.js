20e33150e0d225384776c3395ea76c82fec0b267
Suleiman <suleimanj515@gmail.com>
2025-03-07 13:31:19 -0800

    Restore deleted file

---
+++ client/fighthub_mobile/keys/keys.js
@@ -0,0 +1,6 @@
+export const CLIENT_ID_WEB =
+  "897991379182-1jo0giu9dphc2gtl1j1giteunhc7c65e.apps.googleusercontent.com";
+export const CLIENT_ID_IOS =
+  "897991379182-3v86rigfejloadk6t9tu6sopon31110r.apps.googleusercontent.com";
+export const CLIENT_ID_ANDROID =
+  "897991379182-74arqn3n0ejnghcptu355d8hv27h2c10.apps.googleusercontent.com";
