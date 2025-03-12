455a5f2daf59fd1b449016e40f96b207b7028abd
Suleiman <suleimanj515@gmail.com>
2025-03-12 10:26:41 -0700

    image and video upload

--- client/fighthub_mobile/screens/App2.js
+++ client/fighthub_mobile/screens/App2.js
@@ -5,6 +5,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "./Profile";
 import Settings from "./Settings"; // Import Settings page
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Home from "./Home";
+import Upload from "./Upload";
 const myTabs = createBottomTabNavigator();
 
 export default function Dashboard() {
@@ -21,6 +23,10 @@ export default function Dashboard() {
             iconName = focused ? "person" : "person-outline";
           } else if (route.name === "Settings") {
             iconName = focused ? "settings" : "settings-outline";
+          } else if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Upload") {
+            iconName = focused ? "add" : "add-outline";
           }
 
           // Return the Ionicons component with the determined name and color
@@ -29,14 +35,24 @@ export default function Dashboard() {
       })}
     >
       <myTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <myTabs.Screen
+        name="Upload"
+        component={Upload}
+        options={{ headerShown: false }}
+      />
+      <myTabs.Screen
         name="Profile"
         component={Profile}
-        options={{ headerShown: false }} // Hide header for Profile
+        options={{ headerShown: false }}
       />
       <myTabs.Screen
         name="Settings"
         component={Settings}
-        options={{ headerShown: false }} // Hide header for Settings
+        options={{ headerShown: false }}
       />
     </myTabs.Navigator>
   );
