6c1f59fb4b35aed52f6b1f281219aa4d6a1a3e99
Suleiman <suleimanj515@gmail.com>
2026-02-03 15:17:35 -0800

    bug fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { sendOtpEmail } from "../service/emailService";
+import { sendOtpEmail } from "../service/emailService.js";
 import crypto from "crypto";
 import { supabaseAdmin } from "../config/supabase";
 
