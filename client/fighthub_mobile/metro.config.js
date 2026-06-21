9affae2741ed4398be5d4f0d6d751e911da2111b
Suleiman <suleimanj515@gmail.com>
2026-06-21 14:53:03 -0400

    app updates

--- client/fighthub_mobile/metro.config.js
+++ client/fighthub_mobile/metro.config.js
@@ -1,11 +1,5 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
-
-// Keep this if you need it for some packages
-config.resolver.sourceExts.push("cjs");
-
-// Leave package exports ON. Turning it off can cause wrong entry resolution.
-config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
